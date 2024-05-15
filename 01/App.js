8ea54e72474b99a64ade8b3ed78f095496d764d2
Abhijeet Kumar <akumar1@advisor360.com>
2024-05-15 21:28:46 +0530

    episode-one

---
+++ 01/App.js
@@ -0,0 +1,14 @@
+// const heading = React.createElement('div',{}, React.createElement('h1',{}, 'Hello World from React'));
+parent = React.createElement("div", {}, [
+    React.createElement("div", {}, [
+      React.createElement("h1", {}, "Hello World from React"),
+      React.createElement("h2", {}, "Hello World from React")
+    ]),
+    React.createElement("div", {}, [
+      React.createElement("h1", {}, "Hello World from React"),
+      React.createElement("h2", {}, "Hello World from React")
+    ])
+  ]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(root);
+root.render(parent);
